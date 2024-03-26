76b36904492665ff30ee114dfee90b4516853b6c
Vorontsov-722a <Oleksandr.Vorontsov@infiz.khpi.edu.ua>
2024-03-26 21:28:52 +0200

    Add project files.

---
+++ Classes task/Company.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Задача_на_класи
+{
+    internal class Company
+    {
+        private string name; private int year;
+        private string country; private string CEO;
+        public int number_of_workers; public Company(string name, int year, string country, string CEO, int number_of_workers)
+        {
+            this.name = name;
+            this.year = year; this.country = country;
+            this.CEO = CEO; this.number_of_workers = number_of_workers;
+        }
+        public void Showinfo()
+        {
+            Console.WriteLine($"Company name:{name}");
+            Console.WriteLine($"Year of foundation:{year}"); Console.WriteLine($"CEO of company:{CEO}");
+            Console.WriteLine($"Company country:{country}"); Console.WriteLine($"Number of workers:{number_of_workers}");
+        }
+        public void Actualinfo(int num_wor)
+        {
+            int k; k = number_of_workers - num_wor;
+
+            if (k == 0) Console.WriteLine($"the number has not changed"); if (k < 0) { Console.WriteLine($"the number of workers increased by {Math.Abs(k)}"); }
+            if (k > 0) { Console.WriteLine($"the number of workers decreased by {Math.Abs(k)}"); }
+            Console.WriteLine($"Number of workers {num_wor}\n");
+            number_of_workers = num_wor;
+        }
+    }
+}
---
+++ Classes task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
+using Задача_на_класи;
+var Company = new Company(name: "Feel Good inc.", year: 1999, country: "USA", CEO: "Waldemar Von Braun", number_of_workers: 25);//оголошення класу "Company"
+Company.Showinfo();//використання методу "Showinfo" для показу інформації
+int num_wor;
+int k;
+
+
+
+    Console.WriteLine("How many people curently work?");//ввід актуальної кількості працівників компанії
+    num_wor = Convert.ToInt32(Console.ReadLine());
+    if (num_wor < 0)
+    {
+        do
+        {
+            Console.WriteLine("Incorrect data, try again" + "\n");
+            num_wor = Convert.ToInt32(Console.ReadLine());
+        } while (num_wor < 0);
+    }
+    Company.Actualinfo(num_wor);//використання методу для оновлення інформації про кількість працівників
+    Console.WriteLine("press 0 to end program, press 1 to continue");
+    k = Convert.ToInt32(Console.ReadLine());
+
